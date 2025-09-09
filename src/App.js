097625e8b7669b58134f167383dbafd904d12567
Aditi Joshi <aditijoshi7811@gmail.com>
2025-09-08 23:48:46 -0600

    Setup Avatar component (#2)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import SpinningBorderAvatar from './components/SpinningBorderAvatar/SpinningBorderAvatar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <SpinningBorderAvatar
+        src="/profile.jpeg"
+        size={250}
+        speed={8}
+        borderWidth={6}
+      />
     </div>
   );
 }
