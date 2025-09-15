85f14c3933f00ae890cde1ef7cd5e765814319ab
Aditi Joshi <aditijoshi7811@gmail.com>
2025-09-15 12:37:07 -0600

    video converted to webm (#13)
    Co-authored-by: Aditi Joshi <aditijoshi@Aditis-MacBook-Air.local>

--- src/components/TiltingCard/TiltingCard.jsx
+++ src/components/TiltingCard/TiltingCard.jsx
@@ -79,7 +79,7 @@ const TiltingCard = ({ title, description, imageUrl, liveSiteUrl, techStackIcons
           
           {/* Live Site Link */}
           <div className="flex items-center space-x-2 font-mono text-green-400">
-            <span>Check Live Site</span>
+            <span>Check Demo</span>
             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7l5 5m0 0l-5 5m5-5H6" />
             </svg>
